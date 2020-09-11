548955984c514ed419fefcb467a1d7d85a6f046d
Michael Fellinger <michael.fellinger@iohk.io>
2020-09-11 20:19:11 +0200

    initial config

--- overlay.nix
+++ overlay.nix
@@ -1,21 +1,25 @@
 { system, self }:
 final: prev: {
-  # inject vault-bin into bitte wrapper
-  bitte = let
-    bitte-nixpkgs = import self.inputs.nixpkgs {
-      inherit system;
-      overlays = [
-        (final: prev: {
-          vault-bin = self.inputs.bitte.legacyPackages.${system}.vault-bin;
-        })
-        self.inputs.bitte-cli.overlay.${system}
-      ];
-    };
-  in bitte-nixpkgs.bitte;
+  # we cannot specify mantis as a flake input due to:
+  # * the branch having a slash
+  # * the submodules syntax is broken
+  # And here we cannot specify simply a branch since that's not reproducible,
+  # so we use the commit instead.
+  # The branch was `chore/update-sbt-add-nix`, for future reference.
+  mantis-source = builtins.fetchGit {
+    url = "https://github.com/input-output-hk/mantis";
+    rev = "0ebe794e2fe20b877e6c96c4e825b73c4e6668d7";
+    submodules = true;
+  };
 
-  nixFlakes = self.inputs.bitte.legacyPackages.${system}.nixFlakes;
+  mantis = import final.mantis-source {
+    inherit system;
+  };
+
+  nomadJobs = final.callPackage ./jobs/mantis.nix { };
 
   devShell = prev.mkShell {
+    # for bitte-cli
     LOG_LEVEL = "debug";
 
     buildInputs = [
@@ -31,6 +35,19 @@ final: prev: {
     ];
   };
 
+  # inject vault-bin into bitte wrapper
+  bitte = let
+    bitte-nixpkgs = import self.inputs.nixpkgs {
+      inherit system;
+      overlays = [
+        (final: prev: {
+          vault-bin = self.inputs.bitte.legacyPackages.${system}.vault-bin;
+        })
+        self.inputs.bitte-cli.overlay.${system}
+      ];
+    };
+  in bitte-nixpkgs.bitte;
+
   nixosConfigurations =
     self.inputs.bitte.legacyPackages.${system}.mkNixosConfigurations
     final.clusters;
@@ -40,10 +57,7 @@ final: prev: {
     inherit self system;
   };
 
-  nomadJobs = final.callPackage ./jobs/mantis.nix { };
-
   inherit (self.inputs.bitte.legacyPackages.${system})
-    vault-bin mkNomadJob mkNomadTaskSandbox terraform-with-plugins;
-
-  systemdSandbox = final.callPackage ./jobs/sandbox.nix { };
+    vault-bin mkNomadJob mkNomadTaskSandbox terraform-with-plugins
+    systemdSandbox nixFlakes;
 }
