b6d514b5b99746d986f1d947add0fd7c258ef3ec
Michael Fellinger <michael.fellinger@iohk.io>
2020-10-09 19:13:03 +0200

    better proxying

--- clusters/mantis/testnet/ingress.nix
+++ clusters/mantis/testnet/ingress.nix
@@ -1,21 +1,43 @@
 { ... }: {
   services.ingress.extraConfig = ''
-    {{ range services -}}
-      {{ if .Tags | contains "public" }}
-        {{ range service .Name }}
-    backend  {{ .ID | replaceAll "-" "_" }}
-      mode tcp
-      server {{ .ID }} {{ .Address }}:{{ .Port }}
-        {{ end }}
+    backend mantis_1
+      default-server resolve-prefer ipv4 resolvers consul resolve-opts allow-dup-ip
+      server mantis-1 mantis-1.mantis-miner.service.consul
 
-        {{ range service .Name }}
-    frontend {{ .ID | replaceAll "-" "_" }}
+    backend mantis_2
+      default-server resolve-prefer ipv4 resolvers consul resolve-opts allow-dup-ip
+      server mantis-2 mantis-2.mantis-miner.service.consul
+
+    backend mantis_3
+      default-server resolve-prefer ipv4 resolvers consul resolve-opts allow-dup-ip
+      server mantis-3 mantis-3.mantis-miner.service.consul
+
+    backend mantis_4
+      default-server resolve-prefer ipv4 resolvers consul resolve-opts allow-dup-ip
+      server mantis-4 mantis-4.mantis-miner.service.consul
+
+    frontend mantis_1
       mode tcp
       option tcplog
-      bind *:{{ .ServiceMeta.Port }}
-      default_backend {{ .ID | replaceAll "-" "_" }}
-        {{ end }}
-      {{- end }}
-    {{- end }}
+      bind *:9001
+      default_backend mantis_1
+
+    frontend mantis_2
+      mode tcp
+      option tcplog
+      bind *:9002
+      default_backend mantis_2
+
+    frontend mantis_3
+      mode tcp
+      option tcplog
+      bind *:9003
+      default_backend mantis_3
+
+    frontend mantis_4
+      mode tcp
+      option tcplog
+      bind *:9004
+      default_backend mantis_4
   '';
 }
