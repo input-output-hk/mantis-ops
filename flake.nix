6249b80c09cbb5a49aaef35ed4cc8938e511813a
Shay Bergmann <sbergmann@kreisys.co>
2020-10-13 07:20:23 -0400

    Add darwin

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
   };
 
   outputs = { self, nixpkgs, utils, ops-lib, bitte, ... }:
-    (utils.lib.eachSystem [ "x86_64-linux" ] (system: rec {
+    (utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system: rec {
       overlay = import ./overlay.nix { inherit system self; };
 
       legacyPackages = import nixpkgs {
