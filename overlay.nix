88ed3bd1de8058baf7ebd59ae1ae9464bbec9e6b
mk <mirko.alic@iohk.io>
2020-11-06 08:56:33 -0300

    Bump mantis with latest version (w/ nix build fixed)

--- overlay.nix
+++ overlay.nix
@@ -10,7 +10,7 @@ in {
   # The branch was `chore/update-sbt-add-nix`, for future reference.
   mantis-source = builtins.fetchGit {
     url = "https://github.com/input-output-hk/mantis";
-    rev = "5db9fb9618f33525d7d152b4a22276415d52d2f2";
+    rev = "62c00140d34e134dcb71b1812e5f76d8401ee747";
     ref = "develop";
     submodules = true;
   };
