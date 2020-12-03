95f96ba0f224aec99c09c01b7a0acd0e16e7529b
Félix Baylac-Jacqué <felix@alternativebit.fr>
2020-12-03 18:08:29 +0100

    Morpho: update morpho node pin

--- flake.nix
+++ flake.nix
@@ -11,6 +11,7 @@
     utils.url = "github:numtide/flake-utils";
     ops-lib.url = "github:input-output-hk/ops-lib/zfs-image?dir=zfs";
     inclusive.follows = "bitte/inclusive";
+    morpho-node.url = "github:input-output-hk/ECIP-Checkpointing/master";
     mantis-automation = {
       url =
         "git+ssh://git@github.com/input-output-hk/mantis-automation?ref=etcm-99-mantis-agen";
