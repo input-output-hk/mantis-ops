4fbd99545dc2283f83d7d78ae4716c782191508f
John Lotoski <john.lotoski@iohk.io>
2020-12-23 13:44:31 -0500

    Age out loki logs at 28 days

--- clusters/mantis/testnet/monitoring-server.nix
+++ clusters/mantis/testnet/monitoring-server.nix
@@ -3,4 +3,8 @@
     name = "provisioned-mantis-ops";
     options.path = ../../../contrib/dashboards;
   }];
+  services.loki.configuration.table_manager = {
+    retention_deletes_enabled = true;
+    retention_period = "28d";
+  };
 }
