input-output-hk/mantis-ops

commit 67504a676ec2e9830e1b63798a2a8fc478fd4655

Distinguish between passive mantis nodes
Previously, all passive nodes were bunched together in loki and grafana

jobs/mantis-staging.nix
@@ -33,7 +33,9 @@ let
 
   mkMantis = { name, resources, count ? 1, templates, serviceName, tags ? [ ]
     , serverMeta ? { }, meta ? { }, discoveryMeta ? { }, requiredPeerCount
-    , services ? { } }: {
+    , services ? { } }:
+    let fullName = name + lib.optionalString (count > 1) "-\${NOMAD_ALLOC_INDEX}";
+    in {
       inherit count;
 
       networks = [{
@@ -93,7 +95,7 @@ let
             omit_hostname = false
 
             [global_tags]
-            client_id = "${name}"
+            client_id = "${fullName}"
             namespace = "${namespace}"
 
             [inputs.prometheus]
@@ -172,7 +174,7 @@ let
           logging = {
             type = "journald";
             config = [{
-              tag = name;
+              tag = fullName;
               labels = "name,namespace,imageTag";
             }];
           };
@@ -294,8 +296,8 @@ let
       };
     });
 
-  mkPassive = count:
-    let name = "${namespace}-mantis-passive";
+  mkPassive = nameSuffix: count:
+    let name = "mantis${lib.optionalString (nameSuffix != null) "-${nameSuffix}"}-passive";
     in mkMantis {
       inherit name;
       serviceName = name;
@@ -921,7 +923,7 @@ in {
 
     taskGroups = let
       minerTaskGroups = lib.listToAttrs (map mkMiner miners);
-      passiveTaskGroups = { passive = mkPassive 3; };
+      passiveTaskGroups = { passive = mkPassive null 3; };
     in minerTaskGroups // passiveTaskGroups;
   };
 
@@ -935,7 +937,7 @@ in {
     taskGroups = let
       mkMorpho = import ./tasks/morpho.nix;
       generateMorphoTaskGroup = nbNodes: node:
-        lib.nameValuePair node.name (lib.recursiveUpdate (mkPassive 1)
+        lib.nameValuePair node.name (lib.recursiveUpdate (mkPassive node.name 1)
           (mkMorpho (node // { inherit lib nbNodes; })));
       morphoTaskGroups =
         map (generateMorphoTaskGroup (builtins.length morphoNodes)) morphoNodes;

commit 945bfc2ede1b597593c25586e049746149464d7b

Fix mantis-explorer in staging
Commit 67504a676ec2e9830e1b63798a2a8fc478fd4655 broke this, because the
explorer relied on the mantis node's serviceName staying the same, which changed in that
commit

jobs/mantis-staging.nix
@@ -300,7 +300,7 @@ let
     let name = "mantis${lib.optionalString (nameSuffix != null) "-${nameSuffix}"}-passive";
     in mkMantis {
       inherit name;
-      serviceName = name;
+      serviceName = "${namespace}-${name}";
       resources = {
         # For c5.2xlarge in clusters/mantis/testnet/default.nix, the url ref below
         # provides 3.4 GHz * 8 vCPU = 27.2 GHz max.  80% is 21760 MHz.
@@ -315,12 +315,6 @@ let
       inherit count;
 
       requiredPeerCount = builtins.length miners;
-
-      services."${name}-rpc" = {
-        addressMode = "host";
-        tags = [ "rpc" namespace name mantis-version ];
-        portLabel = "rpc";
-      };
 
       templates = [
         {
@@ -386,7 +380,7 @@ let
       addressMode = "host";
       portLabel = "explorer";
 
-      tags = [ namespace "explorer" name ];
+      tags = [ "ingress" namespace "explorer" name ];
 
       meta = {
         inherit name;