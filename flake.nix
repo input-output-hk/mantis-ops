21559e9353788c2a2e5066889f5f9d781a42d4eb
Craige McWhirter <craige.mcwhirter@iohk.io>
2021-03-09 19:37:39 +1000

    bitte: Bumped to HEAD of terraform-state-in-vault

--- flake.nix
+++ flake.nix
@@ -2,14 +2,13 @@
   description = "Bitte for Mantis";
 
   inputs = {
-    bitte-cli.follows = "bitte/bitte-cli";
-    bitte.url = "github:input-output-hk/bitte";
+    bitte.url = "github:input-output-hk/bitte/terraform-state-in-vault";
     # bitte.url = "path:/home/craige/source/IOHK/bitte";
     # bitte.url = "path:/home/jlotoski/work/iohk/bitte-wt/bitte";
     # bitte.url = "path:/home/manveru/github/input-output-hk/bitte";
     nixpkgs.follows = "bitte/nixpkgs";
     terranix.follows = "bitte/terranix";
-    utils.url = "github:numtide/flake-utils";
+    utils.follows = "bitte/utils";
     ops-lib.url = "github:input-output-hk/ops-lib/zfs-image?dir=zfs";
     inclusive.follows = "bitte/inclusive";
     morpho-node.url = "github:input-output-hk/ECIP-Checkpointing/master";
@@ -17,11 +16,8 @@
     mantis-faucet-web.url = "github:input-output-hk/mantis-faucet-web";
   };
 
-  outputs = { self, nixpkgs, utils, ops-lib, bitte, ... }@inputs:
+  outputs = { self, nixpkgs, ops-lib, bitte, ... }@inputs:
     let
-      mantisKevmOverlay = import ./overlay.nix inputs;
-      bitteOverlay = bitte.overlay.x86_64-linux;
-
       hashiStack = bitte.mkHashiStack {
         flake = self;
         rootDir = ./.;
@@ -33,15 +29,24 @@
         system = "x86_64-linux";
         overlays = [
           (final: prev: { inherit (hashiStack) clusters dockerImages; })
-          bitteOverlay
-          mantisKevmOverlay
+          bitte.overlay.x86_64-linux
+          (import ./overlay.nix inputs)
         ];
       };
+
+      nixosConfigurations = hashiStack.nixosConfigurations;
     in {
-      inherit self;
-      inherit (hashiStack) nomadJobs dockerImages clusters nixosConfigurations;
-      inherit (pkgs) sources;
+      inherit nixosConfigurations;
+      clusters.x86_64-linux = hashiStack.clusters;
       legacyPackages.x86_64-linux = pkgs;
       devShell.x86_64-linux = pkgs.devShell;
+      hydraJobs.x86_64-linux = {
+        inherit (pkgs)
+          devShellPath bitte nixFlakes sops
+          terraform-with-plugins cfssl consul nomad vault-bin cue grafana
+          haproxy grafana-loki victoriametrics
+          ;
+      } // (pkgs.lib.mapAttrs (_: v: v.config.system.build.toplevel)
+        nixosConfigurations);
     };
 }
