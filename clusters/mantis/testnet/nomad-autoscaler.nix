86dd7065b8f68cb2840d85dfe2d0142a378f559d
Timothy DeHerrera <tim.deherrera@iohk.io>
2021-04-22 13:13:04 -0600

    tune min/max for autoscaler

--- clusters/mantis/testnet/nomad-autoscaler.nix
+++ clusters/mantis/testnet/nomad-autoscaler.nix
@@ -1,3 +1,16 @@
-{ self, ... }: {
+{ self, config, lib, ... }:
+let
+  eachASG = lib.flip lib.mapAttrs config.cluster.autoscalingGroups;
+in {
   imports = [ (self.inputs.bitte + /profiles/nomad/autoscaler.nix) ];
+
+  services.nomad-autoscaler.policies = eachASG (name: asg: {
+    min = 1;
+    max = 10;
+
+    policy.check = {
+      mem_allocated_percentage.strategy.target-value.target = 70.0;
+      cpu_allocated_percentage.strategy.target-value.target = 70.0;
+    };
+  });
 }
