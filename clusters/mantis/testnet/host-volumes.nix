e7b61f65f7f755a84ff55ab40818a0f8f6fb9d14
Timothy DeHerrera <tim.deherrera@iohk.io>
2021-06-02 17:22:21 -0600

    remove gluster volume in prep for csi

--- clusters/mantis/testnet/host-volumes.nix
+++ clusters/mantis/testnet/host-volumes.nix
@@ -1,30 +1,9 @@
-{ pkgs, config, lib, self, ... }: {
-  imports = [ (self.inputs.bitte + /profiles/glusterfs/client.nix) ];
-
+{ config, lib, ... }: {
   services.nomad.client = {
     chroot_env = {
       "/etc/passwd" = "/etc/passwd";
       "/etc/resolv.conf" = "/etc/resolv.conf";
       "/etc/services" = "/etc/services";
     };
-
-    host_volume = [{
-      rocksdb = {
-        path = "/mnt/gv0/nomad/rocksdb";
-        read_only = false;
-      };
-    }];
   };
-
-  system.activationScripts.nomad-host-volumes-new = ''
-    export PATH="${lib.makeBinPath (with pkgs; [ fd coreutils ])}:$PATH"
-  '' + (lib.pipe config.services.nomad.client.host_volume [
-    (map builtins.attrNames)
-    builtins.concatLists
-    (map (d: ''
-      mkdir -p /mnt/gv0/nomad/${d}
-      fd . -o root /mnt/gv0/nomad/${d} -X chown nobody:nogroup
-    ''))
-    (builtins.concatStringsSep "\n")
-  ]);
 }
