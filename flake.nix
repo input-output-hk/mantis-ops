6d889fa2e5dcf95bc558b31b3dd7549294558bf7
Michael Fellinger <michael.fellinger@iohk.io>
2021-10-06 09:28:33 +0200

    bump bitte-cli

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     bitte.url = "github:input-output-hk/bitte";
-    bitte.inputs.bitte-cli.url = "github:input-output-hk/bitte-cli/v0.3.5";
+    bitte.inputs.bitte-cli.url = "github:input-output-hk/bitte-cli";
     nixpkgs.follows = "bitte/nixpkgs";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     terranix.follows = "bitte/terranix";
@@ -21,71 +21,45 @@
       inherit nixpkgs;
       systems = [ "x86_64-linux" ];
 
-      preOverlays = [
-        bitte.overlay
-      ];
+      preOverlays = [ bitte.overlay ];
 
       overlay = import ./overlay.nix inputs;
 
-      packages =
-        { generate-mantis-keys
-        , mantis-faucet-nginx
-        , mantis-explorer-nginx
-        , restic-backup
-        , mantis-faucet-server
-        , checkFmt
-        , devShellPath
-        }@pkgs: pkgs;
+      packages = { generate-mantis-keys, mantis-faucet-nginx
+        , mantis-explorer-nginx, restic-backup, mantis-faucet-server, checkFmt
+        , devShellPath }@pkgs:
+        pkgs;
 
-      devShell = { bitteShellCompat, cue }: bitteShellCompat {
-        inherit self;
-        extraPackages = [ cue ];
-        cluster = "mantis-testnet";
-        profile = "mantis";
-        region = self.clusters."mantis-testnet".proto.config.cluster.region;
-        domain = "mantis.ws";
-        nixConfig = ''
-          extra-substituters = https://hydra.mantis.ist
-          extra-trusted-public-keys = hydra.mantis.ist-1:4LTe7Q+5pm8+HawKxvmn2Hx0E3NbkYjtf1oWv+eAmTo=
-        '';
-      };
+      devShell = { bitteShellCompat, cue }:
+        bitteShellCompat {
+          inherit self;
+          extraPackages = [ cue ];
+          cluster = "mantis-testnet";
+          profile = "mantis";
+          region = self.clusters."mantis-testnet".proto.config.cluster.region;
+          domain = "mantis.ws";
+          nixConfig = ''
+            extra-substituters = https://hydra.mantis.ist
+            extra-trusted-public-keys = hydra.mantis.ist-1:4LTe7Q+5pm8+HawKxvmn2Hx0E3NbkYjtf1oWv+eAmTo=
+          '';
+        };
 
-      extraOutputs =
-        let hashiStack = bitte.lib.mkHashiStack {
+      extraOutputs = let
+        hashiStack = bitte.lib.mkHashiStack {
           flake = self;
           domain = "mantis.ws";
         };
-        in
-        {
-          inherit (hashiStack) clusters nixosConfigurations consulTemplates;
-        } // bitte.lib.mkDeploy { inherit self; };
+      in {
+        inherit (hashiStack) clusters nixosConfigurations consulTemplates;
+      } // bitte.lib.mkDeploy { inherit self; };
 
-      hydraJobs =
-        { bitte
-        , cfssl
-        , consul
-        , cue
-        , devShellPath
-        , grafana
-        , grafana-loki
-        , haproxy
-        , nixFlakes
-        , nomad
-        , sops
-        , terraform-with-plugins
-        , vault-bin
-        , victoriametrics
-        , generate-mantis-keys
-        , mantis
-        , mantis-staging
-        , mantis-faucet-web
-        , mantis-faucet-nginx
-        , mantis-faucet-server
-        , mantis-explorer
-        , mantis-explorer-nginx
-        , morpho-node
-        , morpho-node-entrypoint
-        }@jobs: jobs // (builtins.mapAttrs (_: v: v.config.system.build.toplevel)
+      hydraJobs = { bitte, cfssl, consul, cue, devShellPath, grafana
+        , grafana-loki, haproxy, nixFlakes, nomad, sops, terraform-with-plugins
+        , vault-bin, victoriametrics, generate-mantis-keys, mantis
+        , mantis-staging, mantis-faucet-web, mantis-faucet-nginx
+        , mantis-faucet-server, mantis-explorer, mantis-explorer-nginx
+        , morpho-node, morpho-node-entrypoint }@jobs:
+        jobs // (builtins.mapAttrs (_: v: v.config.system.build.toplevel)
           self.nixosConfigurations);
     };
 }
